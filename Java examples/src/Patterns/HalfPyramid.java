9c55534387911f9306b12e3a8c2852ae25f41173
SAI PALLAVI <SAI PALLAVI@Pallavi>
2024-11-05 12:22:30 +0530

    patterns

---
+++ Java examples/src/Patterns/HalfPyramid.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+public class HalfPyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+			
+		}
+		System.out.println();
+		
+
+	}
+
+}
--- Java examples/src/Patterns/RectangleStar.java
+++ Java examples/src/Patterns/RectangleStar.java
@@ -10,13 +10,7 @@ public class RectangleStar {
           {
         	  for(int j=1;j<=m;j++)
               {
-        		  if(i==1||i==n||j==1||j==m)
-        		  {
-            	  System.out.print("*");
-        		  }else
-        		  {
-        			  System.out.print(" ");
-        		  }
+        		  System.out.print("*");
         
               }
         	  System.out.println("*");
